c286134c248887151ff936ca45484296fbf53efa
2591255234@qq.com <woaini@521>
2020-04-04 17:55:52 +0800

    '项目初始化'

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.less';
+function App() {
+  return (
+    <div className="App">
+      11
+    </div>
+  );
+}
+export default App;
